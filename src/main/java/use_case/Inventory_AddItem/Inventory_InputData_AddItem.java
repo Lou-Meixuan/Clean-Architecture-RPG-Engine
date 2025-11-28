4d2132ceaa2735b7537db13b8de52f78eebb1316
i-2049 <evaisabelmelo@gmail.com>
2025-11-28 09:41:02 -0500

    All files for usecase of Inventory_AddItem

--- src/main/java/use_case/Inventory_AddItem/Inventory_InputData_AddItem.java
+++ src/main/java/use_case/Inventory_AddItem/Inventory_InputData_AddItem.java
@@ -1,9 +1,9 @@
-package use_case.Inventory;
+package use_case.Inventory_AddItem;
 
 public class Inventory_InputData_AddItem {
-    private final String item_name;
-    public Inventory_InputData_AddItem(String item_name)
-    {this.item_name=item_name;}
-    public String getItem_name()
-    {return item_name;}
+    private final String itemName;
+    public Inventory_InputData_AddItem(String itemName)
+    {this.itemName = itemName;}
+    public String getItemName()
+    {return itemName;}
 }
